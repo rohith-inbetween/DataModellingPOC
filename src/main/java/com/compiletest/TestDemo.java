1dd7c3c54f244a819631705087e1ada72dd4a352
tauseef-inbetween <tauseef.ahmad@inbetween.co.in>
2015-02-05 20:48:30 +0530

    Code refactoring done

--- src/main/java/com/compiletest/TestDemo.java
+++ src/main/java/com/compiletest/TestDemo.java
@@ -24,17 +24,15 @@ public class TestDemo {
 	
 	public static void loadClass()throws Exception{
 		// The dir contains the compiled classes. 
-		File classesDir = new File("D:/TestSpring/TestCompile/classes");
+		File classesDir = new File("src/createdClasses/");
 
 		// The parent classloader 
 		ClassLoader parentLoader = ClassLoader.getSystemClassLoader();
 
 		URLClassLoader loader1 = new URLClassLoader( new URL[] { classesDir.toURI().toURL() }, parentLoader);
-		Class cls1 = Class.forName("com.compiletest.CompileIt");
-//		Class cls1 = loader1.loadClass("CompileIt");
+//		Class cls1 = Class.forName("com.compiletest.CompileIt");
+		Class cls1 = loader1.loadClass("com.compiletest.CompileIt");
 		Object obj = cls1.newInstance();
 		obj.getClass().getMethod("sayHello").invoke(obj);
-//		obj.sayHello();
-	
 	}
 }
