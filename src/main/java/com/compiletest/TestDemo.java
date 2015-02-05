9c3d1e3f64cca5d97498c81735142a08f9028214
tauseef-inbetween <tauseef.ahmad@inbetween.co.in>
2015-02-05 21:26:46 +0530

    Refactoring done for Class.forName

--- src/main/java/com/compiletest/TestDemo.java
+++ src/main/java/com/compiletest/TestDemo.java
@@ -1,6 +1,7 @@
 package com.compiletest;
 
 import java.io.File;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 
@@ -24,15 +25,27 @@ public class TestDemo {
 	
 	public static void loadClass()throws Exception{
 		// The dir contains the compiled classes. 
-		File classesDir = new File("src/createdClasses/");
+//		File classesDir = new File("src/createdClasses/");
 
 		// The parent classloader 
-		ClassLoader parentLoader = ClassLoader.getSystemClassLoader();
-
-		URLClassLoader loader1 = new URLClassLoader( new URL[] { classesDir.toURI().toURL() }, parentLoader);
-//		Class cls1 = Class.forName("com.compiletest.CompileIt");
-		Class cls1 = loader1.loadClass("com.compiletest.CompileIt");
+//		ClassLoader parentLoader = ClassLoader.getSystemClassLoader();
+		
+//		URLClassLoader loader1 = new URLClassLoader( new URL[] { classesDir.toURI().toURL() }, parentLoader);
+		
+		addPath("src/createdClasses/");
+		Class cls1 = Class.forName("com.compiletest.CompileIt");
+//		Class cls1 = loader1.loadClass("com.compiletest.CompileIt");
 		Object obj = cls1.newInstance();
 		obj.getClass().getMethod("sayHello").invoke(obj);
+	}
+	
+	public static void addPath(String s) throws Exception {
+	    File f = new File(s);
+	    URL u = f.toURI().toURL();
+	    URLClassLoader urlClassLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+	    Class urlClass = URLClassLoader.class;
+	    Method method = urlClass.getDeclaredMethod("addURL", new Class[]{URL.class});
+	    method.setAccessible(true);
+	    method.invoke(urlClassLoader, new Object[]{u});
 	}
 }
