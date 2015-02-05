614da1bcecf8462565c232894e58d4555fcc3f77
Rohith Premachandran <rohith@inbetween.com>
2015-02-05 17:57:59 +0530

    POC create tables DONE!

--- src/test/unittest/com/hibernatetest/TestCreateTable.java
+++ src/test/unittest/com/hibernatetest/TestCreateTable.java
@@ -22,42 +22,15 @@ public class TestCreateTable {
 		
 		Configuration config = new Configuration();
 		Properties props = new Properties();
-		  props.put(Environment.DIALECT,"org.hibernate.dialect.MySQLInnoDBDialect");
+		  props.put(Environment.DIALECT,"org.hibernate.dialect.MySQLDialect");
 		  props.put(Environment.URL,"jdbc:mysql://localhost:3306/datamodelling");
 		  props.put(Environment.DRIVER,"com.mysql.jdbc.Driver");
 		  props.put(Environment.USER,"root");
 		  props.put(Environment.PASS,"");
-		
-		props.put("hibernate.hbm2ddl.auto", "create");
-//		props.put("hibernate.dialect", "org.hibernate.dialect.MySQLInnoDBDialect");
-//		props.put("hibernate.connection.provider_class",
-//		 "com.zutubi.pulse.upgrade.tasks.UpgradeTaskConnectionProvider");
-//		config.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
-//		config.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/datamodelling");
-//		config.setProperty("hibernate.connection.username", "root");
-//	    config.setProperty("hibernate.connection.password", "");
-//	    config.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLInnoDBDialect");
-		
 		  config.addAnnotatedClass(TableToBeAdded.class);
-//		}
-config.addProperties(props);
-		// run the schema update.
-	SchemaUpdate update=new SchemaUpdate(config);
-//	update.setDelimiter(";");
-//	  update.setHaltOnError(true);
-//	  update.setOutputFile("src/main/resources/update.sql");
-//	  update.setFormat(true);
-	  update.execute(true, true);
-//		  Dialect dialect = Dialect.getDialect(props);
-//		  Connection connection = dataSource.getConnection();
-//		  DatabaseMetadata meta = 
-//		      new DatabaseMetadata(connection, dialect);
-//		  String[] createSQL = 
-//		      config.generateSchemaUpdateScript(dialect, meta);
-//		  System.out.println(createSQL[0]);
-//		  connection.close();
-		  
-		
+		  config.addProperties(props);
+		  SchemaUpdate update=new SchemaUpdate(config);
+		  update.execute(true, true);
 	}
 	
 }
